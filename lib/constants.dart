096fdfecf8a8a0e5d7ebbd1c362bc1b40b2adacf
Marcos Sevilla <marcossevilla14@gmail.com>
2020-08-29 19:41:38 -0600

    added constants

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+const kPrimaryYellow = Color(0xffFFCC66);
+const kPrimaryGrey = Color(0xff282828);
+const kSecondaryRed = Color(0xffE46472);
+const kSecondaryGreen = Color(0xff309397);
+const kSecondaryBlue = Color(0xff6488E4);
+const kSecondaryOrange = Color(0xffF9BE7C);
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app_clone/constants.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({
-    Key key,
-  }) : super(key: key);
+  const HomeScreen({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Todo App Bar'),
-      ),
-      body: Center(
-        child: Container(
-          child: Text('Hello World'),
-        ),
+      body: Column(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height * 0.2,
+            color: kPrimaryYellow,
+          )
+        ],
       ),
     );
   }
